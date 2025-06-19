e47c9b0f14a7bd3f0a618378f5b5b4908de4ad43
Hugo Visser <hugo@littlerobots.nl>
2025-06-19 20:37:41 +0200

    Move off old versions + clean up
    * Bump Gradle and dependency versions
    * Relocate kotlin version using gr8
    * Clean up left behind classes

--- plugin/src/main/kotlin/nl/littlerobots/vcu/plugin/resolver/VersionSelectors.kt
+++ plugin/src/main/kotlin/nl/littlerobots/vcu/plugin/resolver/VersionSelectors.kt
@@ -18,7 +18,7 @@ package nl.littlerobots.vcu.plugin.resolver
 class VersionSelectors {
     companion object {
         private fun isStable(version: String): Boolean {
-            val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+            val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
             val regex = "^[0-9,.v-]+(-r)?$".toRegex()
             val isStable = stableKeyword || regex.matches(version)
             return isStable
